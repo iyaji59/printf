7aaa305c1dccb92b522e847a07dfa93dbfe0d140
tonybruce <iyajianthony59@gmail.com>
2023-04-22 11:06:43 -0700

    task 0

--- main.h
+++ main.h
@@ -1,91 +1,117 @@
-#ifndef HOLBERTONH
-#define HOLBERTONH
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
- * struct specifier - contains specifiers for printf
+ * struct fmt - Struct op
  *
- * @width: width of field to print
- * @precision: precision of field to print
- * @length: modifies type length. h/l flags. 0 default, +1 per l, -1 per h
- * @widthflag: tells whether width is set
- * @precisionflag: tells whether precision is set
- * @left: left justify, '-'. 0 for right, 1 for left
- * @sign: force sign, '+'. 0 for no, 1 for yes
- * @space: space pad left side of positive numbers if leading 0s. 0 no 1 yes
- * @zero: zero pad numbers up to width. 0 for no, 1 for yes.
- * @zerox: add leading 0, 0x, or 0X to o, x or X specifiers. 0 no 1 yes
- * @specifier: the data type to print, c, s, d, x, etc.
+ * @fmt: The format.
+ * @fn: The function associated.
  */
-typedef struct specifier
+struct fmt
 {
-	unsigned int width;
-	unsigned int precision;
-	int length;
-	char widthflag;
-	char precisionflag;
-	char left;
-	char sign;
-	char space;
-	char zero;
-	char zerox;
-	char specifier;
-} specifier;
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
-/* from printf.c */
-int _printf(char *format, ...);
 
-/* from prep_types.c */
-char *prep_string(char *str, specifier spec);
-char *prep_numeric(char *str, specifier spec);
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
 
-/* from utility.c */
-int _strlen(char *str);
-int ifputs(char *s);
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/* from itos.c */
-char *itos(va_list list);
-char *hitos(va_list list);
-char *litos(va_list list);
-char *hhitos(va_list list);
+/****************** FUNCTIONS ******************/
 
-/* from uitob.c */
-char *uitob(va_list list);
-char *luitob(va_list list);
-char *huitob(va_list list);
-char *hhuitob(va_list list);
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/* from utos.c */
-char *utos(va_list list);
-char *hutos(va_list list);
-char *hhutos(va_list list);
-char *lutos(va_list list);
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/* from itoo.c */
-char *itoo(va_list list);
-char *litoo(va_list list);
-char *hitoo(va_list list);
-char *hhitoo(va_list list);
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* from itox.c */
-char *itox(va_list list);
-char *litox(va_list list);
-char *hitox(va_list list);
-char *hhitox(va_list list);
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/* from itox.c */
-char *itoX(va_list list);
-char *hitoX(va_list list);
-char *hhitoX(va_list list);
-char *litoX(va_list list);
+/* Funcion to print memory address */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/* from stos.c */
-char *rev(char *str);
-char *rot(char *str);
-char *print_hidden(char *str);
-char *null(void);
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
-/* from ptos.c */
-char *nil(void);
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-#endif
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
